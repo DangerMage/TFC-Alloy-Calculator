7f44ea1650ea9dfb77d01072b6c405f48ad46e1a
DangerMage <benjamin+github@grusendorf.family>
2024-03-12 16:06:45 -0600

    Parsing config into list of records

--- src/main/java/org/dangermage/ConfigManager.java
+++ src/main/java/org/dangermage/ConfigManager.java
@@ -1,26 +1,20 @@
 package org.dangermage;
 
 
+import com.electronwill.nightconfig.core.Config;
+import com.electronwill.nightconfig.core.file.FileConfig;
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConfigManager {
-    public File saveResource(String name) throws IOException {
-        return saveResource(name, true);
-    }
 
-    public File saveResource(String name, boolean replace) throws IOException {
-        return saveResource(new File("."), name, replace);
-    }
-
-    public File saveResource(File outputDirectory, String name) throws IOException {
-        return saveResource(outputDirectory, name, true);
-    }
-
-    public File saveResource(File outputDirectory, String name, boolean replace)
+    public void saveResource(File outputDirectory, String name, boolean replace)
             throws IOException {
         File out = new File(outputDirectory, name);
         if (!replace && out.exists())
-            return out;
+            return;
         // Step 1:
         InputStream resource = this.getClass().getResourceAsStream(name);
         if (resource == null)
@@ -36,6 +30,48 @@ public class ConfigManager {
                 writer.write(buffer, 0, length);
             }
         }
-        return out;
     }
+
+    public List<Alloy> readConfig(FileConfig config) {
+        List<Alloy> definedAlloys = new ArrayList<>();
+
+        if (!config.contains("alloys")) {
+            return null;
+        }
+
+        ArrayList<Config> alloys = config.get("alloys");
+
+        for (Config alloy : alloys) {
+
+            List<Material> parsedMaterials = new ArrayList<>();
+
+            String name = alloy.get("name");
+
+            ArrayList<Config> materials = alloy.get("components");
+
+            for (Config material : materials) {
+                String materialName = material.get("material");
+                int minPercentWhole = material.get("minPercentage");
+                int maxPercentWhole = material.get("maxPercentage");
+
+                double minPercent = (double) minPercentWhole / 100;
+
+                double maxPercent = (double) maxPercentWhole / 100;
+
+
+                Material parsedMaterial = new Material(materialName, minPercent, maxPercent);
+
+                parsedMaterials.add(parsedMaterial);
+            }
+
+            Alloy parsedAlloy = new Alloy(name, parsedMaterials);
+
+            definedAlloys.add(parsedAlloy);
+
+
+        }
+
+        return definedAlloys;
+    }
+
 }
